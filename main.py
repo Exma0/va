421272ac4f00dd42259df188ed7243dccc0fd7d8
Exma0 <oztoprakyilmazcan@gmail.com>
2026-01-11 13:35:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -16,13 +16,21 @@ adapter = requests.adapters.HTTPAdapter(pool_connections=1000, pool_maxsize=1000
 session.mount("http://", adapter)
 session.mount("https://", adapter)
 
-UA = 'VAVOO/2.6'
+CHROME_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+
 COMMON_HEADERS = {
-    "User-Agent": UA,
-    "Accept": "*/*",
+    "User-Agent": CHROME_UA,
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
-    "Accept-Encoding": "gzip, deflate",
-    "Referer": "https://vavoo.to/"
+    "Referer": "https://vavoo.to/",
+    "Origin": "https://vavoo.to",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "cross-site",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache"
 }
 
 CACHE_DATA = {"list": None, "time": 0}
@@ -37,7 +45,9 @@ def get_random_ip():
 @lru_cache(maxsize=2048)
 def resolve_url(url):
     try:
-        r = session.get(url, headers=COMMON_HEADERS, timeout=4)
+        headers = COMMON_HEADERS.copy()
+        headers["X-Forwarded-For"] = get_random_ip()
+        r = session.get(url, headers=headers, timeout=4)
         if "#EXT-X-STREAM-INF" in r.text:
             lines = r.text.splitlines()
             streams = [l.strip() for l in lines if l and not l.startswith("#")]
@@ -55,7 +65,7 @@ def root_playlist():
     try:
         headers = COMMON_HEADERS.copy()
         headers["X-Forwarded-For"] = get_random_ip()
-        r = session.get("https://vavoo.to/live2/index", headers=headers, timeout=8)
+        r = session.get("https://vavoo.to/live2/index", headers=headers, timeout=10)
         data = r.text
     except:
         return "ERROR", 500
@@ -87,7 +97,7 @@ def live_m3u8():
     try:
         headers = COMMON_HEADERS.copy()
         headers["X-Forwarded-For"] = get_random_ip()
-        r = session.get(target_url, headers=headers, timeout=5)
+        r = session.get(target_url, headers=headers, timeout=6)
         
         if r.status_code != 200: return "SRC ERR", 403
 
